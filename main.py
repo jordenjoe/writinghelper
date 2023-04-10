60748597abfc2b765eaa9f869a7285983be9228a
Jorden Jolley <94325574+jordenjoe@users.noreply.github.com>
2023-04-10 12:30:41 -0400

    labeled letters, added zoom

--- main.py
+++ main.py
@@ -82,18 +82,9 @@ def generate_spacing_feedback(bounding_rectangles):
 
     print(spacings)
 
-def generate_letter_feedback(img):
+def generate_bounding_rectangles(contours, img):
 
-    # TODO: NEED A WAY TO MAINTAIN LETTER ORDER
-     
-
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    ret, th1 = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
-
-
-    # get the contours, narrow down
-    contours, hierarchy = cv.findContours(th1, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
-
+     # get the main 5 letters written on the user page
     areas = []
 
     for contour in contours:
@@ -107,30 +98,125 @@ def generate_letter_feedback(img):
 
     print("top 5: ", top_5)
 
+    # get the bounding rectangles
+
     bounding_rectangles = []
 
+    i = 0
     for contour in contours:
 
         if cv.contourArea(contour) in top_5:
 
             # Get the bounding rectangle
             x, y, w, h = cv.boundingRect(contour)
-            # Draw the rectangle on the original image
-            cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
             bounding_rectangles.append([x,y,w,h])
 
-    sorted_bounding_rectangles = sorted(bounding_rectangles, key=lambda rect: rect[0])
+            i += 1
 
-    plt.imshow(img)
+    print('bounding rectangles: ', bounding_rectangles)
+
+    return sorted(bounding_rectangles, key=lambda rect: rect[0])
+
+def generate_letter_labels(written_letter_information, img):
+
+    labels = ["Letter 1", "Letter 2", "Letter 3", "Letter 4", "Letter 5"]
+
+    i = 0
+    for letter in written_letter_information:
+            
+        x = written_letter_information[letter]["Bounding Rectangle"][0]
+        y = written_letter_information[letter]["Bounding Rectangle"][1]
+        w = written_letter_information[letter]["Bounding Rectangle"][2]
+        h = written_letter_information[letter]["Bounding Rectangle"][3]
+
+        # TODO: change text and rectangle color based on feedback - orange, green, or red
+
+        # Draw the rectangle on the original image
+        cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # Add the label to the rectangle
+        label = labels[i]
+        cv.putText(img, label, (x, y-10), cv.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 0), 2)
+        
+        i += 1
+
+def generate_letter_information(sorted_bounding_rectangles):
+
+    written_letter_information = {}
+
+    # put the written letters into a dictionary by order left to right
+    # with bounding rectangles and areas
+
+    i = 0
+    for bounding_rectangle in sorted_bounding_rectangles:
+        written_letter_information[i] = {"Bounding Rectangle":bounding_rectangle,
+                                         "Area":bounding_rectangle[2]*bounding_rectangle[3]}
+        i += 1
+
+    for item in written_letter_information:
+        print(item, written_letter_information[item])
+
+    return written_letter_information
+
+def generate_image_zoom(img, sorted_bounding_rectangles):
+
+    # x, y, w, h
+    # x is the furthest left x (min x) - 100
+    # y is the furthest up y (min y) - 100
+    # w is the furthest right x (max x) - (min x) + 100
+    # h is the max y - min y + 100 
+
+    new_list = []
+    for i in range(2):
+        column = []
+        for j in range(len(sorted_bounding_rectangles)):
+            column.append(sorted_bounding_rectangles[j][i])
+        new_list.append(column)
+
+    x_vals = new_list[0]
+    y_vals = new_list[1]
+
+    new_x = max(min(x_vals)-100,0)
+    new_y = max(min(y_vals)-100,0)
+    new_w = max(x_vals) - new_x + 300
+    new_h = max(y_vals) - new_y + 300
+
+    roi = img[new_y:new_y+new_h, new_x:new_x+new_w]
+
+    # Resize the ROI
+    zoom_level = 2
+    zoomed_roi = cv.resize(roi, (new_w*zoom_level, new_h*zoom_level))
+
+    return zoomed_roi
+
+def generate_letter_feedback(img):
+     
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    ret, th1 = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+
+    # get the contours, narrow down
+    contours, hierarchy = cv.findContours(th1, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+
+    sorted_bounding_rectangles = generate_bounding_rectangles(contours, img)
+    written_letter_information = generate_letter_information(sorted_bounding_rectangles)
+
+    #generate_size_feedback(top_5)
+    #time.sleep(3)
+    #generate_spacing_feedback(sorted_bounding_rectangles)
+    #time.sleep(3)
+
+    # add labels to each letter, in sorted order.
+    # change color based on feedback scores
+    generate_letter_labels(written_letter_information, img)
+
+    # zoom in on image so user can see better
+    zoomed_roi = generate_image_zoom(img, sorted_bounding_rectangles)
+
+    plt.imshow(zoomed_roi)
     plt.title("Contours")
     plt.show()
      
-    generate_size_feedback(top_5)
-    time.sleep(3)
-    generate_spacing_feedback(sorted_bounding_rectangles)
-    time.sleep(3)
-
     return 1
 
 def letter_analysis(letter):
