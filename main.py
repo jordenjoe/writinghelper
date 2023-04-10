57696099d8f0eb569b2dbda0935a96e327297fe7
Jorden Jolley <94325574+jordenjoe@users.noreply.github.com>
2023-04-10 14:20:31 -0400

    updated from using size/spacing mean to median, bc mean was getting dragged by outlier

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import statistics
 
 
 practice_letters = ['L', 'F', 'E', 'H', 'T', 'I', 'D', 'B', 'P', 'U', 'J', 'C', 'O', 'G', 'Q', 'S', 'R', 'A', 'K', 'M', 'N', 'V', 'W', 'X', 'Y', 'Z', 'l', 't', 'i', 'c', 'k', 'o', 'p', 's', 'v', 'u', 'w', 'x', 'z', 'h', 'n', 'm', 'r', 'b', 'a', 'd', 'g', 'q', 'e', 'f', 'j', 'k', 'y']
-practice_letters = ['L', 'F']
+practice_letters = ['L', 'F', 'E']
 TK_SILENCE_DEPRECATION=1
 
 def get_letter_image(letter):
@@ -33,35 +33,42 @@ def get_letter_image(letter):
 
     return img   
 
-def generate_size_feedback(top_5):
+def generate_size_feedback(written_letter_information):
 
-    size_feedback_score = 0
+    area_list = [sub_dict['Area'] for sub_dict in written_letter_information.values()]
 
-    average = statistics.mean(top_5)
 
-    for index, number in enumerate(top_5):
+    size_feedback_scores = []
+
+    average = statistics.median(area_list)
+
+    for index, number in enumerate(area_list):
 
         if number > average + (average/3):
 
             print(f"Letter {index+1} is too large compared to your other letters.",
                    "Try writing it smaller. ")
+            size_feedback_scores.append(0)
             
         elif number < average - (average/3):
 
             print(f"Letter {index+1} is too small compared to your other letters.",
                    "Try writing it larger. ")
+            size_feedback_scores.append(0)
             
         else: 
-            size_feedback_score += 1
+            size_feedback_scores.append(1)
     
-    if size_feedback_score == 5:
-        print(f"Good letter size for all letters.")
+    if sum(size_feedback_scores) == 5:
+        print(f"Reasonable letter size for all letters.")
 
-def generate_spacing_feedback(bounding_rectangles):
+    return size_feedback_scores
 
-    print('bounding_rectangles: ', bounding_rectangles)
+def generate_spacing_feedback(written_letter_information):
 
-    spacing_feedback_score = 0
+    # print('written_letter_information: ', written_letter_information)
+
+    spacing_feedback_scores = [0,0,0,0,0]
     spacings = []
 
     # spacing between:
@@ -71,16 +78,32 @@ def generate_spacing_feedback(bounding_rectangles):
     # 4 and 5
     for i in range(4):
 
-        firstLetterRight = bounding_rectangles[i][0]+bounding_rectangles[i][2]
-        secondLetterLeft = bounding_rectangles[i+1][0]
-        print(bounding_rectangles[i][0]+bounding_rectangles[i][2])
-        print(bounding_rectangles[i+1][0])
+        firstLetterRight = written_letter_information[i]["Bounding Rectangle"][0] +\
+                            written_letter_information[i]["Bounding Rectangle"][2]
+
+        secondLetterLeft = written_letter_information[i+1]["Bounding Rectangle"][0]
+
         spacing = secondLetterLeft-firstLetterRight
 
         spacings.append(spacing)
 
+    average = statistics.median(spacings)
 
-    print(spacings)
+    for index in range(4):
+        spacing = spacings[index]
+
+        # if not within 50 of average, flag spacing for both letters 
+        if (average - 50 < spacing < average + 50):
+
+            spacing_feedback_scores[index] = 1
+            spacing_feedback_scores[index+1] = 1
+
+        else:
+            print('Letter ', index+1, ' and letter ', index+2, ' have incorrect relative spacing')
+            spacing_feedback_scores[index] = 0
+            spacing_feedback_scores[index+1] = 0
+
+    return spacing_feedback_scores
 
 def generate_bounding_rectangles(contours, img):
 
@@ -96,7 +119,7 @@ def generate_bounding_rectangles(contours, img):
     # Get the top 5 contours
     top_5 = sorted_areas[1:6]
 
-    print("top 5: ", top_5)
+    # print("top 5: ", top_5)
 
     # get the bounding rectangles
 
@@ -114,11 +137,11 @@ def generate_bounding_rectangles(contours, img):
 
             i += 1
 
-    print('bounding rectangles: ', bounding_rectangles)
+    # print('bounding rectangles: ', bounding_rectangles)
 
     return sorted(bounding_rectangles, key=lambda rect: rect[0])
 
-def generate_letter_labels(written_letter_information, img):
+def generate_letter_labels(written_letter_information, img, total_feedback_scores):
 
     labels = ["Letter 1", "Letter 2", "Letter 3", "Letter 4", "Letter 5"]
 
@@ -131,12 +154,19 @@ def generate_letter_labels(written_letter_information, img):
         h = written_letter_information[letter]["Bounding Rectangle"][3]
 
         # TODO: change text and rectangle color based on feedback - orange, green, or red
+        if total_feedback_scores[i] == 2:
+            color = (0, 255, 0) # green
+        elif total_feedback_scores[i] == 0:
+            color = (255, 0, 0) # red
+        else:
+            color = (255, 165, 0) # orange
 
         # Draw the rectangle on the original image
-        cv.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        cv.rectangle(img, (x, y), (x+w, y+h), color, 2)
 
         # Add the label to the rectangle
         label = labels[i]
+
         cv.putText(img, label, (x, y-10), cv.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 0), 2)
         
         i += 1
@@ -154,8 +184,8 @@ def generate_letter_information(sorted_bounding_rectangles):
                                          "Area":bounding_rectangle[2]*bounding_rectangle[3]}
         i += 1
 
-    for item in written_letter_information:
-        print(item, written_letter_information[item])
+    # for item in written_letter_information:
+    #   print(item, written_letter_information[item])
 
     return written_letter_information
 
@@ -179,8 +209,8 @@ def generate_image_zoom(img, sorted_bounding_rectangles):
 
     new_x = max(min(x_vals)-100,0)
     new_y = max(min(y_vals)-100,0)
-    new_w = max(x_vals) - new_x + 300
-    new_h = max(y_vals) - new_y + 300
+    new_w = max(x_vals) - new_x + 500
+    new_h = max(y_vals) - new_y + 500
 
     roi = img[new_y:new_y+new_h, new_x:new_x+new_w]
 
@@ -201,20 +231,27 @@ def generate_letter_feedback(img):
     sorted_bounding_rectangles = generate_bounding_rectangles(contours, img)
     written_letter_information = generate_letter_information(sorted_bounding_rectangles)
 
-    #generate_size_feedback(top_5)
-    #time.sleep(3)
-    #generate_spacing_feedback(sorted_bounding_rectangles)
-    #time.sleep(3)
+    # 1 point possible per letter
+    size_feedback_scores = generate_size_feedback(written_letter_information)
+
+    # 1 point possible per letter
+    spacing_feedback_scores = generate_spacing_feedback(written_letter_information)
+
+    print('size_feedback_scores: ', size_feedback_scores)
+    print('spacing_feedback_scores: ', spacing_feedback_scores)
+
+    # total feedback score 
+    total_feedback_scores = [size_feedback_scores[i] + spacing_feedback_scores[i] for i in range(len(size_feedback_scores))]
 
     # add labels to each letter, in sorted order.
     # change color based on feedback scores
-    generate_letter_labels(written_letter_information, img)
+    generate_letter_labels(written_letter_information, img, total_feedback_scores)
 
     # zoom in on image so user can see better
     zoomed_roi = generate_image_zoom(img, sorted_bounding_rectangles)
 
     plt.imshow(zoomed_roi)
-    plt.title("Contours")
+    plt.title("Feedback")
     plt.show()
      
     return 1
@@ -225,12 +262,11 @@ def letter_analysis(letter):
         
         img = get_letter_image(letter)
         print('Thanks for uploading your image!'\
-        '\nGenerating feedback.....')
+        '\nGenerating feedback.....\n')
         generate_letter_feedback(img)
 
         # automating a decimal for now
         return .5
-
 
 def main():
 
@@ -245,11 +281,11 @@ def main():
             result = letter_analysis(letter)
 
             if result < threshold:
-                 print('Your result ', result, ' was less than your threshold ', threshold,\
+                 print('\n\nYour result ', result, ' was less than your threshold ', threshold,\
                        "\nLet's get more practice!")
                  time.sleep(5)
             else:
-                print('Your result ', result, ' was greater than your threshold! Congrats.')
+                print('\n\nYour result ', result, ' was greater than your threshold! Congrats.')
                 break
 
         print('Do you want to continue onto the next letter? Y/N')
